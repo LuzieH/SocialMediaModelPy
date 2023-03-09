2ca311216bd07a24504680d383e481b847e314ec
Helfmann <luziehelfmann@gmail.com>
2023-03-09 20:27:06 +0100

    added gitignore

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='SocialMediaModelPy',
    version='0.1.0',
-   author='Luzie Helfmann',
+   author='Luzie Helfmann, Kristina Maier, Nayely Velez-Cruz, Inan Bostanci, Adam Wiechman',
    author_email='luziehelfmann@gmail.com',
    description='An awesome package that does something',
    long_description=open('README.md').read(),
