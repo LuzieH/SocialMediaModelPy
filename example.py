b0bd42c91a1ea17d6b84ef1c2517da25263fc160
Helfmann <luziehelfmann@gmail.com>
2023-03-09 19:49:10 +0100

    added theta values to example.py

--- example.py
+++ example.py
@@ -9,7 +9,8 @@ framespath = "img/frames"
 N = 250 # number of individuals
 timesteps = 1000 # time steps to simulate with a stepsize of dt ##350
 a = 0.5 
-theta = 2.9 # repulsion: 2.5, 2, 1, 0.5 attraction: 3 , interesting 2.9
+theta_inf = 2.9 # repulsion: 2.5, 2, 1, 0.5 attraction: 3 , interesting 2.9
+theta_ind = 2.9
 seed = 0 # seed for random number generator
 sigma = 0.1
 
@@ -17,7 +18,7 @@ sigma = 0.1
 x0,z0,A,C0,D = abm.initialcondition(N, L=4, seed=seed)
 
 # instantiate
-ops = abm.opinions(x0, z0, A, C0,D, a=a, theta=theta,sigma=sigma) 
+ops = abm.opinions(x0, z0, A, C0,D, a=a, theta_inf=theta_inf,theta_ind= theta_ind,sigma=sigma) 
 
 # evolve model
 xs,zs,Cs = ops.run(timesteps=timesteps, seed=seed)
